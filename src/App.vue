749f6cc39263226bf01e35451b868da5668ab250
Enfield-Li <1076462153@qq.com>
2022-04-11 10:29:34 +0800

    hack

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
+import { toRefs } from "vue-demi";
 import { RouterLink, RouterView } from "vue-router";
+import Routes from "./router/Routes.vue";
+import { useCounterStore } from "./stores/counter";
+const counterStore = useCounterStore();
+
+const {counter, doubleCount, increment} = toRefs( counterStore );
 </script>
 
 <template>
   <header>
     <div class="wrapper">
       <h1>Hello world</h1>
+      <p>{{counter}}</p>
+      <p>{{doubleCount}}</p>
+      <button @click="increment">increse</button>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <Routes />
     </div>
   </header>
 
